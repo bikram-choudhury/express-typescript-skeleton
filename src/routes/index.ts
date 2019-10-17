e88940418475399951fffd71d91d9b35f55352a4
Bikram Choudhury <bikram.choudhury527@gmail.com>
2019-10-17 10:21:48 +0530

    Initial commit with basic typescript conversion

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response, NextFunction, IRouter } from "express";
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.render('index', { title: 'Express' });
+});
+
+export default router;
